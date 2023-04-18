cd0e0d10e85703c3bed79026c2dbceb1aa4555d4
Mohammad Alsharif <malshari@outlook.com>
2023-04-17 23:31:35 -0500

    Formatting

--- Retail/Utils/Networking/NetworkingManager.swift
+++ Retail/Utils/Networking/NetworkingManager.swift
@@ -19,10 +19,12 @@ class NetworkingManager {
     }
 
     static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
-        guard let response = output.response as? HTTPURLResponse,
-              response.statusCode >= 200 && response.statusCode < 300 else {
-                  throw NetworkingError.badURLResponse(url: url)
-              }
+        guard
+            let response = output.response as? HTTPURLResponse,
+            response.statusCode >= 200 && response.statusCode < 300
+        else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
 
         return output.data
     }
